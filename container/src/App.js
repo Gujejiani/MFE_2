fbdcefc377704e48665660794efa032e80687071
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-02-22 17:03:53 +0400

    region corrected

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/marketingApp'
 
 export default ()=>{
     return  <div>
-        <h1 >Container is running !!</h1>
+        <h1 >Container is running !!!!</h1>
         <hr/>
         <MarketingApp/>
     </div>
