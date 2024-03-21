219629515883d6c420a9967f39ecf6e5106b84b1
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-03-21 14:19:17 +0400

    bug fixed

--- dashboard/config/webpack.dev.js
+++ dashboard/config/webpack.dev.js
@@ -1,39 +1,36 @@
-const {merge } = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const commonConfig = require('./webpack.common.js');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const devConfig = {
-    mode: 'development',
-    output: {
-        publicPath: 'http://localhost:8083/'
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8083/',
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      historyApiFallback: true,
     },
-    devServer: {
-       
-        port: 8083,
-        historyApiFallback: {
-            index: '/index.html'
-        },
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        }
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
-        new ModuleFederationPlugin({
-            name: 'dashboard',
-            filename: 'remoteEntry.js',
-            exposes: {
-               './DashboardApp': './src/bootstrap'
-            },
-            shared: packageJson.dependencies
-        })
-    ]
-
-}
-
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'dashboard',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DashboardApp': './src/bootstrap',
+      },
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
 
 module.exports = merge(commonConfig, devConfig);
